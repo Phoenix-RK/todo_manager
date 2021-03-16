6b3f9dbd661237bc761764d33e195bca881966f4
Phoenix-RK <kiruthyasp31@gmail.com>
2021-03-16 20:01:18 +0530

    Added a route that shows a static message

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "todos", to: "todos#index"
 end
+
+
+
+#routes.rb is the place where we define every URL that our application is supposed to handle.
